89521fcd5f5d53f7a481f9699dd011fe7ae5c1b1
YipHoiChun <yiphoichun29@gmail.com>
2021-04-17 18:16:17 +0800

    add responsive css

--- e-commerce/src/components/products/ProductForm.vue
+++ e-commerce/src/components/products/ProductForm.vue
@@ -21,17 +21,18 @@
       </div>
       <div class="form-group">
         <label>Manufacturer</label>
-        <select
-          type="text"
-          class="form-control"
-          v-model="model.manufacturer"
-          name="manufacturer">
+        <select type="text" class="form-control" v-model="model.manufacturer" name="manufacturer">
           <template v-for="manufacturer in manufacturers">
-            <option :value="manufacturer._id" :selected="manufacturer._id == (model.manufacturer && model.manufacturer._id)">{{manufacturer.name}}</option>
+            <option
+              :key="manufacturer._id"
+              :value="manufacturer._id"
+              :selected="manufacturer._id == (model.manufacturer && model.manufacturer._id)"
+            >{{manufacturer.name}}</option>
           </template>
         </select>
       </div>
     </div>
+
 
     <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
       <div class="form-group">
--- e-commerce/src/components/products/ProductItem.vue
+++ e-commerce/src/components/products/ProductItem.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <div class="product">
+      <p class="product__name">Name：{{ product.name }}</p>
+      <img :src="product.image" alt="" class="product__image" />
       <router-link :to="'/detail/' + product._id" class="product-link">
-        <p class="product__name">Name：{{product.name}}</p>
-        <p class="product__description">Description：{{product.description}}</p>
-        <p class="product__price">Price：{{product.price}}</p>
-        <p class="product.manufacturer">Manufacturer：{{product.manufacturer.name}}</p>
-        <img :src="product.image" alt="" class="product__image">
+        <!-- <p class="product__name">Name：{{ product.name }}</p> -->
+        <p class="product__description">
+          Description：{{ product.description }}
+        </p>
+        <p class="product__price">Price：{{ product.price }}</p>
+        <p class="product.manufacturer">
+          Manufacturer：{{ product.manufacturer.name }}
+        </p>
+        <!-- <b-img :src="product.image" fluid alt="Responsive image"></b-img> -->
       </router-link>
       <product-button :product="product"></product-button>
     </div>
@@ -15,23 +21,51 @@
 
 <style>
 .product {
+  padding: 20px;
   border-bottom: 1px solid black;
-  float: inline-start 
+}
+.product__name{
+  font-size: 30px;
+}
+
+.product__description {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  white-space: normal;
 }
 
 .product__image {
-  width: 125px;
-  height: 125px;
+  width: 50%;
+  height: 50%;
+}
+
+@media screen and (min-width: 601px) {
+  .product {
+    font-size: 30px;
+  }
+  .product__name{
+  font-size: 50px;
+}
+}
+
+@media screen and (max-width: 600px) {
+  .product {
+    font-size: 30px;
+  }
 }
 </style>
 
 <script>
-import ProductButton from './ProductButton';
+import ProductButton from "./ProductButton";
 export default {
-  name: 'product-item',
-  props: ['product'],
+  name: "product-item",
+  props: ["product"],
   components: {
-    'product-button': ProductButton,
-  }
-}
+    "product-button": ProductButton,
+  },
+};
 </script>
