83e3519b47f38729848d5aaa7b0d1ff26a208246
YipHoiChun <yiphoichun29@gmail.com>
2021-04-17 11:25:30 +0800

    install boostrap-vue

--- e-commerce/src/main.js
+++ e-commerce/src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import { ValidationProvider } from 'vee-validate';
-
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App';
 import router from './router';
 import store from './store';
-
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
 Vue.component('ValidationProvider', ValidationProvider);
 
